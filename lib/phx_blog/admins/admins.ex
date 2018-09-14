75146f7747de8d55bba8db58378a384a87f856a4
takuru <asaumi@kray.jp>
2018-09-14 15:37:27 +0900

    Adminモデル作成

---
+++ lib/phx_blog/admins/admins.ex
@@ -0,0 +1,5 @@
+defmodule PhxBlog.Admins do
+  import Ecto.Query, warn: false
+  alias PhxBlog.Repo
+  alias PhxBlog.Admins.Admin
+end
---
+++ priv/repo/migrations/20180914063300_create_admins.exs
@@ -0,0 +1,13 @@
+defmodule PhxBlog.Repo.Migrations.CreateAdmins do
+  use Ecto.Migration
+
+  def change do
+    create table(:admins) do
+      add :email, :string
+      add :password_hash, :string
+
+      timestamps()
+    end
+
+  end
+end
