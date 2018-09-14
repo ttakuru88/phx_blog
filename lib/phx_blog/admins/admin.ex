2c404696ef0bc3f9ab19586791c73c2b3021e524
takuru <asaumi@kray.jp>
2018-09-14 16:40:20 +0900

    Adminにハッシュされたパスワードを設定できる

--- lib/phx_blog/admins/admin.ex
+++ lib/phx_blog/admins/admin.ex
@@ -6,6 +6,7 @@ defmodule PhxBlog.Admins.Admin do
   schema "admins" do
     field :email, :string
     field :password_hash, :string
+    field :password, :string, virtual: true
 
     timestamps()
   end
@@ -13,7 +14,18 @@ defmodule PhxBlog.Admins.Admin do
   @doc false
   def changeset(admin, attrs) do
     admin
-    |> cast(attrs, [:email, :password_hash])
-    |> validate_required([:email, :password_hash])
+    |> cast(attrs, [:email, :password])
+    |> validate_required([:email, :password])
+    |> put_password_hash()
+  end
+
+  defp put_password_hash(changeset) do
+    case changeset do
+      %Ecto.Changeset{valid?: true, changes: %{password: pass}} ->
+        changeset
+        |> put_change(:password_hash, Comeonin.Argon2.hashpwsalt(pass))
+      _ ->
+        changeset
+    end
   end
 end
--- lib/phx_blog/admins/admins.ex
+++ lib/phx_blog/admins/admins.ex
@@ -2,4 +2,6 @@ defmodule PhxBlog.Admins do
   import Ecto.Query, warn: false
   alias PhxBlog.Repo
   alias PhxBlog.Admins.Admin
+
+  def get_admin(id), do: Repo.get(Admin, id)
 end
