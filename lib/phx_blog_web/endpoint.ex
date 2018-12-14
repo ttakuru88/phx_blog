709d534863259f5d583969434d2631324dc44fc9
ttakuru88 <asaumi@kray.jp>
2018-12-14 18:20:09 +0900

    アップロードした画像を/imagesでアクセスできる

--- lib/phx_blog_web/endpoint.ex
+++ lib/phx_blog_web/endpoint.ex
@@ -11,6 +11,10 @@ defmodule PhxBlogWeb.Endpoint do
     at: "/", from: :phx_blog, gzip: false,
     only: ~w(css fonts images js favicon.ico robots.txt)
 
+  plug Plug.Static,
+    at: "/images",
+    from: "#{Application.app_dir(:phx_blog, "priv")}/uploads"
+
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
   if code_reloading? do
